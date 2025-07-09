5c08514d84ef07596caee09e183087a58ed5b940
Keegan Wright <keeganwright12@gmail.com>
2025-07-09 21:17:02 +0100

    SVG warning clean up

--- src/app/components/portfolio-header.tsx
+++ src/app/components/portfolio-header.tsx
@@ -99,7 +99,7 @@ export default function PortfolioHeader() {
                     <svg fill="currentColor" className="h-6 w-6" viewBox="0 0 1024 1024"
                          xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                         <g id="SVGRepo_iconCarrier">
                             <circle cx="512" cy="512" r="512"></circle>
                             <path
